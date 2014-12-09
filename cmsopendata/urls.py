34d9df2a48ce64602d1fb0ca95d104721eb0bf4f
Aeonium <info@aeonium.eu>
2014-12-09 08:50:22 +0000

    Urls file.

--- cmsopendata/urls.py
+++ cmsopendata/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from cmsopendata import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cmsopendata.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('openaccess.urls', namespace="openaccess")),
 )
+
